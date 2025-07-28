94a24f8f10b78c0bee77e5509cf9849b603b76ed
jovitha2808 <jovitha.sj2023@vitstudent.ac.in>
2025-07-28 21:28:06 +0530

    Update placesformpage.jsx

--- client/pages/placesformpage.jsx
+++ client/pages/placesformpage.jsx
@@ -1,51 +1,132 @@
-import {Link, useParams} from "react-router-dom";
+import PhotosUploader from "../PhotosUploader.jsx";
+import Perks from "../Perks.jsx";
 import {useEffect, useState} from "react";
 import axios from "axios";
-import BookingWidget from "../BookingWidget";
-import PlaceGallery from "../PlaceGallery";
-import AddressLink from "../AddressLink";
+import AccountNav from "../AccountNav";
+import {Navigate, useParams} from "react-router-dom";
 
-export default function PlacePage() {
+export default function PlacesFormPage() {
   const {id} = useParams();
-  const [place,setPlace] = useState(null);
+  const [title,setTitle] = useState('');
+  const [address,setAddress] = useState('');
+  const [addedPhotos,setAddedPhotos] = useState([]);
+  const [description,setDescription] = useState('');
+  const [perks,setPerks] = useState([]);
+  const [extraInfo,setExtraInfo] = useState('');
+  const [checkIn,setCheckIn] = useState('');
+  const [checkOut,setCheckOut] = useState('');
+  const [maxGuests,setMaxGuests] = useState(1);
+  const [price,setPrice] = useState(100);
+  const [redirect,setRedirect] = useState(false);
   useEffect(() => {
     if (!id) {
       return;
     }
-    axios.get(`/places/${id}`).then(response => {
-      setPlace(response.data);
+    axios.get('/places/'+id).then(response => {
+       const {data} = response;
+       setTitle(data.title);
+       setAddress(data.address);
+       setAddedPhotos(data.photos);
+       setDescription(data.description);
+       setPerks(data.perks);
+       setExtraInfo(data.extraInfo);
+       setCheckIn(data.checkIn);
+       setCheckOut(data.checkOut);
+       setMaxGuests(data.maxGuests);
+       setPrice(data.price);
     });
   }, [id]);
+  function inputHeader(text) {
+    return (
+      <h2 className="text-2xl mt-4">{text}</h2>
+    );
+  }
+  function inputDescription(text) {
+    return (
+      <p className="text-gray-500 text-sm">{text}</p>
+    );
+  }
+  function preInput(header,description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
 
-  if (!place) return '';
+  async function savePlace(ev) {
+    ev.preventDefault();
+    const placeData = {
+      title, address, addedPhotos,
+      description, perks, extraInfo,
+      checkIn, checkOut, maxGuests, price,
+    };
+    if (id) {
+      // update
+      await axios.put('/places', {
+        id, ...placeData
+      });
+      setRedirect(true);
+    } else {
+      // new place
+      await axios.post('/places', placeData);
+      setRedirect(true);
+    }
 
+  }
 
+  if (redirect) {
+    return <Navigate to={'/account/places'} />
+  }
 
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
-      <h1 className="text-3xl">{place.title}</h1>
-      <AddressLink>{place.address}</AddressLink>
-      <PlaceGallery place={place} />
-      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
-        <div>
-          <div className="my-4">
-            <h2 className="font-semibold text-2xl">Description</h2>
-            {place.description}
+    <div>
+      <AccountNav />
+      <form onSubmit={savePlace}>
+        {preInput('Title', 'Title for your place. should be short and catchy as in advertisement')}
+        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely apt"/>
+        {preInput('Address', 'Address to this place')}
+        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)}placeholder="address"/>
+        {preInput('Photos','more = better')}
+        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+        {preInput('Description','description of the place')}
+        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
+        {preInput('Perks','select all the perks of your place')}
+        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+          <Perks selected={perks} onChange={setPerks} />
+        </div>
+        {preInput('Extra info','house rules, etc')}
+        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
+        {preInput('Check in&out times','add check in and out times, remember to have some time window for cleaning the room between guests')}
+        <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
+          <div>
+            <h3 className="mt-2 -mb-1">Check in time</h3>
+            <input type="text"
+                   value={checkIn}
+                   onChange={ev => setCheckIn(ev.target.value)}
+                   placeholder="14"/>
           </div>
-          Check-in: {place.checkIn}<br />
-          Check-out: {place.checkOut}<br />
-          Max number of guests: {place.maxGuests}
+          <div>
+            <h3 className="mt-2 -mb-1">Check out time</h3>
+            <input type="text"
+                   value={checkOut}
+                   onChange={ev => setCheckOut(ev.target.value)}
+                   placeholder="11" />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Max number of guests</h3>
+            <input type="number" value={maxGuests}
+                   onChange={ev => setMaxGuests(ev.target.value)}/>
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Price per night</h3>
+            <input type="number" value={price}
+                   onChange={ev => setPrice(ev.target.value)}/>
+          </div>
         </div>
-        <div>
-          <BookingWidget place={place} />
-        </div>
-      </div>
-      <div className="bg-white -mx-8 px-8 py-8 border-t">
-        <div>
-          <h2 className="font-semibold text-2xl">Extra info</h2>
-        </div>
-        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
-      </div>
+        <button className="primary my-4">Save</button>
+      </form>
     </div>
   );
 }
